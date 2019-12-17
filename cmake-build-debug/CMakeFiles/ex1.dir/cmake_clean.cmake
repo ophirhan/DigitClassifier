b7724cc47756605b70f80587f30ee844299a2bea
ophir <ophir.han@mail.huji.ac.il>
2019-12-17 18:50:42 +0200

    Intial commit

---
+++ cmake-build-debug/CMakeFiles/ex1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex1.dir/main.cpp.o"
+  "ex1.exe"
+  "ex1.pdb"
+  "libex1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
