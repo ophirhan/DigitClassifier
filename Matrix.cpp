b4549f3f5155afb8cb1534b283a4c3525e34f1d0
ophir <ophir.han@mail.huji.ac.il>
2019-12-25 15:19:12 +0200

    finisheddddd

--- Matrix.cpp
+++ Matrix.cpp
@@ -3,15 +3,21 @@
 //
 #include "Matrix.h"
 
-#define MATRIX_OPERATOR_ERROR "dashjk"
+#define ERROR "Error: "
+#define INVALID_DIMS "Invalid matrix dimensions"
+#define OUT_OF_BOUNDS "Index out of bounds"
 
-
+/**
+ * @breif Creates new matrix object.
+ * @param rows how many rows in the matrix.
+ * @param cols how many cols in the matrix.
+ */
 Matrix::Matrix(const int &rows,const int &cols):
 dims({rows, cols})
 {
     if(rows < 1 || cols < 1)
     {
-        std::cerr << MATRIX_OPERATOR_ERROR << std::endl;
+        std::cerr << ERROR << INVALID_DIMS << std::endl;
         exit(EXIT_FAILURE);
     }
     matrix = new(std::nothrow) float[rows*cols];
@@ -26,11 +32,18 @@ int Matrix::length() const
     return getRows()*getCols();
 }
 
+/**
+ * @breif Creates new matrix object with dims 1 by 1.
+ */
 Matrix::Matrix():
 Matrix(1,1)
 {
 }
 
+/**
+ * @breif Copy constructor for matrix object.
+ * @param other
+ */
 Matrix::Matrix(const Matrix &other):
 matrix(new(std::nothrow) float[other.length()]), dims({other.getRows(), other.getCols()})
 {
@@ -40,27 +53,44 @@ matrix(new(std::nothrow) float[other.length()]), dims({other.getRows(), other.ge
     }
 }
 
+/**
+ * @breif Destructor for Matrix object.
+ */
 Matrix::~Matrix()
 {
     delete [] (matrix);
 }
 
+/**
+ * @breif Getter for dims.rows.
+ * @return number of rows in the matrix.
+ */
 int Matrix::getRows() const
 {
     return dims.rows;
 }
 
+/**
+ * @breif Getter for dims.cols.
+ * @return number of cols in the matrix.
+ */
 int Matrix::getCols() const
 {
     return dims.cols;
 }
-
+/**
+ * @breif Turns a given matrix in vector shape.
+ * @return given matrix as vector.
+ */
 Matrix& Matrix::vectorize()
 {
     dims = {length(), 1};
     return *this;
 }
 
+/**
+ * @breif Prints matrix elements, no return value.
+ */
 void Matrix::plainPrint() const
 {
     for(int i = 0; i < getRows(); i++)
@@ -73,6 +103,10 @@ void Matrix::plainPrint() const
     }
 }
 
+/**
+ * @breif Sets the matrix to be same as given matrix
+ * @return a reference to the matrix.
+ */
 Matrix& Matrix::operator=(const Matrix &other)
 {
     if(other.length() != length())
@@ -85,15 +119,18 @@ Matrix& Matrix::operator=(const Matrix &other)
     {
         (*this)[i] = other[i];
     }
-
     return *this;
 }
 
+/**
+ * @breif Matrix multiplication.
+ * @return a matrix which is the result of matrix multiplication.
+ */
 Matrix Matrix::operator*(const Matrix &right) const
 {
     if(getCols() != right.getRows())
     {
-        std::cerr << MATRIX_OPERATOR_ERROR << std::endl;
+        std::cerr << ERROR << "Matrix multiplication not defined for these dimensions" << std::endl;
         exit(EXIT_FAILURE);
     }
     Matrix result(getRows(), right.getCols());
@@ -110,6 +147,10 @@ Matrix Matrix::operator*(const Matrix &right) const
     return result;
 }
 
+/**
+ * @breif Matrix scalar multiplication.
+ * @return the matrix multiplied by given scalar.
+ */
 Matrix Matrix::operator*(const float &scalar) const
 {
     Matrix result(*this);
@@ -120,11 +161,19 @@ Matrix Matrix::operator*(const float &scalar) const
     return result;
 }
 
+/**
+ * @breif Matrix scalar multiplication.
+ * @return the matrix multiplied by given scalar.
+ */
 Matrix operator*(const float &scalar, const Matrix &mat)
 {
     return mat*scalar;
 }
 
+/**
+ * @breif Matrix addition.
+ * @return the result of addition between this matrix and a given matrix.
+ */
 Matrix Matrix::operator+(const Matrix &other) const
 {
     Matrix result(*this);
@@ -132,53 +181,68 @@ Matrix Matrix::operator+(const Matrix &other) const
     return result;
 }
 
+/**
+ * @return reference to cell in given indexes
+ */
 float& Matrix::operator()(const int row,const int col)
 {
-    int index = row*getCols() + col;
     if(row < 0 || col < 0 || col >= getCols() || row >= getRows())
     {
-        std::cerr << "6855675587" << std::endl;
+        std::cerr << ERROR << OUT_OF_BOUNDS << std::endl;
         exit(EXIT_FAILURE);
     }
-    return (*this)[index];
+    return (*this)[row*getCols() + col];
 }
 
+/**
+ * @return reference to cell in given index
+ */
 float& Matrix::operator[](const int i)
 {
     if(i < 0 || i >= length())
     {
-        std::cerr << "9" << std::endl;
+        std::cerr << ERROR << OUT_OF_BOUNDS << std::endl;
         exit(EXIT_FAILURE);
     }
     return matrix[i];
 }
 
+/**
+ * @return const reference to cell in given indexes
+ */
 const float& Matrix::operator()(const int row,const int col) const
 {
     int index = row*getCols() + col;
     if(row < 0 || col < 0 || col >= getCols() || row >= getRows())
     {
-        std::cerr << "2" << std::endl;
+        std::cerr << ERROR << OUT_OF_BOUNDS << std::endl;
         exit(EXIT_FAILURE);
     }
     return (*this)[index];
 }
 
+/**
+ * @return const reference to cell in given index
+ */
 const float& Matrix::operator[](const int i) const
 {
     if(i < 0 || i >= length())
     {
-        std::cerr << MATRIX_OPERATOR_ERROR << std::endl;
+        std::cerr << ERROR << OUT_OF_BOUNDS << std::endl;
         exit(EXIT_FAILURE);
     }
     return matrix[i];
 }
 
+/**
+ * @breif Sets the matrix to be the result of addition between it and a given matrix.
+ * @return the matrix.
+ */
 Matrix& Matrix::operator+=(const Matrix &other)
 {
     if((*this).getRows() != other.getRows() || (*this).getCols() != other.getCols())
     {
-        std::cerr << MATRIX_OPERATOR_ERROR << std::endl;
+        std::cerr << ERROR << INVALID_DIMS << std::endl;
         exit(EXIT_FAILURE);
     }
     for(int i = 0; i < length(); i++)
@@ -188,6 +252,12 @@ Matrix& Matrix::operator+=(const Matrix &other)
     return *this;
 }
 
+/**
+ * @brief reads a stream to a given matrix.
+ * @param in stream to read
+ * @param mat matrix to fill
+ * @return given stream
+ */
 std::istream& operator>>(std::istream &in, Matrix &mat)
 {
     if(in)
@@ -195,13 +265,19 @@ std::istream& operator>>(std::istream &in, Matrix &mat)
         in.read((char*) mat.matrix, mat.getCols()*mat.getRows()* sizeof(float));
         if(!in.good())
         {
-            std::cerr << MATRIX_OPERATOR_ERROR << std::endl;
+            std::cerr << ERROR << "stream length does not match matrix" << std::endl;
             exit(EXIT_FAILURE);
         }
     }
     return in;
 }
 
+/**
+ * @brief outputs matrix content into a given output stream
+ * @param out output stream
+ * @param mat matrix to read
+ * @return the given stream
+ */
 std::ostream& operator<<(std::ostream& out, Matrix &mat)
 {
     for(int i = 0; i < mat.getRows(); i++)
--- Matrix.h
+++ Matrix.h
@@ -16,113 +16,150 @@ typedef struct MatrixDims
 } MatrixDims;
 
 /**
- * Matrix class for neural network project.
+ * @breif Matrix class for neural network project.
  */
 class Matrix
 {
 public:
     /**
-     * Creates new matrix object.
+     * @breif Creates new matrix object.
      * @param rows how many rows in the matrix.
      * @param cols how many cols in the matrix.
      */
     Matrix(const int &rows, const int &cols);
 
     /**
-     * Creates new matrix object with dims 1 by 1.
+     * @breif Creates new matrix object with dims 1 by 1.
      */
     Matrix();
 
     /**
-     * Copy constructor for matrix object.
+     * @breif Copy constructor for matrix object.
      * @param other
      */
     Matrix(const Matrix &other);
 
     /**
-     * Destructor for Matrix object.
+     * @breif Destructor for Matrix object.
      */
     ~Matrix();
 
     /**
-     * Getter for dims.rows.
+     * @breif Getter for dims.rows.
      * @return number of rows in the matrix.
      */
     int getRows() const;
 
     /**
-     * Getter for dims.cols.
+     * @breif Getter for dims.cols.
      * @return number of cols in the matrix.
      */
     int getCols() const;
 
     /**
-     * Turns a given matrix in vector shape.
+     * @breif Turns a given matrix in vector shape.
      * @return given matrix as vector.
      */
     Matrix& vectorize();
 
     /**
-     * Prints matrix elements, no return value.
+     * @breif Prints matrix elements, no return value.
      */
     void plainPrint() const;
 
     /**
-     * Sets the matrix to be same as given matrix
+     * @breif Sets the matrix to be same as given matrix
      * @return a reference to the matrix.
      */
     Matrix& operator=(const Matrix &);
 
     /**
-     * Matrix multiplication.
+     * @breif Matrix multiplication.
      * @return a matrix which is the result of matrix multiplication.
      */
     Matrix operator*(const Matrix &) const;
 
     /**
-     * Matrix scalar multiplication.
+     * @breif Matrix scalar multiplication.
      * @return the matrix multiplied by given scalar.
      */
     Matrix operator*(const float &) const;
 
     /**
-     * Matrix scalar multiplication.
+     * @breif Matrix scalar multiplication.
      * @return the matrix multiplied by given scalar.
      */
     friend Matrix operator*(const float &, const Matrix &);
 
     /**
-     * Matrix addition.
+     * @breif Matrix addition.
      * @return the result of addition between this matrix and a given matrix.
      */
     Matrix operator+(const Matrix &) const;
 
     /**
-     * Sets the matrix to be the result of addition between it and a given matrix.
+     * @breif Sets the matrix to be the result of addition between it and a given matrix.
      * @return the matrix.
      */
     Matrix& operator+=(const Matrix &);
 
     /**
-     * Returns the float in the corresponding cell of the matrix.
-     * @return
+     * @breif Returns the float in the corresponding cell of the matrix.
+     * @return float
      */
     float& operator()(int, int);
 
+
+    /**
+     * @breif Returns the float in the corresponding cell of the matrix.
+     * @return float
+     */
     float& operator[](int);
 
+
+    /**
+     * @breif Returns the float in the corresponding cell of the matrix.
+     * @return float
+     */
     const float& operator()(int, int) const;
 
+    /**
+     * @breif Returns the float in the corresponding cell of the matrix.
+     * @return float
+     */
     const float& operator[](int) const;
 
+    /**
+     * @breif Reads an input stream into a matrix.
+     * @param in the stream.
+     * @param mat the matrix.
+     * @return the stream.
+     */
     friend std::istream& operator>>(std::istream &in, Matrix &mat);
 
+    /**
+     * @breif Outputs the content of a matrix to a given stream.
+     * @param out the stream.
+     * @param mat the matrix to output
+     * @return the stream.
+     */
     friend std::ostream& operator<<(std::ostream &out, Matrix &mat);
 
 
 private:
+    /**
+     * @return the amount of cells in the matrix.
+     */
     int length() const;
+
+    /**
+     * @breif pointer to the first cell in the matrix.
+     */
     float *matrix;
+
+    /**
+     * @breif Struct with how many rows and columns in the matrix.
+     */
     MatrixDims dims;
 };
 
